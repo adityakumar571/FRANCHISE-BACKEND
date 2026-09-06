56c97b67b3214bf10408c07c6377ccc1da53b7a7
Adiauctech <Aditya.kumar@auctech.in>
2026-09-06 13:25:13 +0530

    backend clean code

--- newrelic.cjs
+++
@@ -1,9 +0,0 @@
-exports.config = {
-    app_name: ['LMS Backend'],
-
-    license_key: 'eu01x82fb4963b120a2b7f87964b6b893bd9NRAL',
-
-    logging: {
-        level: 'info',
-    },
-}
--- utils/generateOTP.js
+++
@@ -1,4 +0,0 @@
-// utils/generateOTP.js
-export const generateOTP = () => {
-    return Math.floor(1000 + Math.random() * 9000).toString(); // Generates a 4-digit OTP
-};
